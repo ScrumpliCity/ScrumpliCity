12feb373162fa181b08c49246539f636c9cdaaec
dasechtelima <0198@htl.rennweg.at>
2024-08-24 15:42:34 +0200

    projektwebsite layout without much styling

--- diplomarbeit/app.vue
+++ diplomarbeit/app.vue
@@ -1,6 +1,59 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="bg-sc-white flex flex-col">
+    <header class="flex items-end w-screen h-12 mb-14 bg-sc-white justify-between underline px-24 fixed">
+      <p>Projekt</p>
+      <p>Ziele</p>
+      <p>Team</p>
+    </header>
+    <main class="flex flex-col items-center mt-28">
+      <h1 class="text-3xl">
+        Willkommen bei
+      </h1>
+      <img src="assets/img/schriftlogo.svg" class="mx-12 my-2"
+           alt="Schriftlogo des Projekts ScrumpliCity, es zeigt den Schriftzug ScrumpliCity mit einer Illustration von 3 Häusern welche von einem Kreis, welcher an Scrum erinnert, umkreist sind">
+      <p class="font-bold text-sc-orange mt-12 text-2xl mb-6">
+        Unser Projekt
+      </p>
+      <p class="text-justify text-sm leading-5 mx-8">
+        Im Rahmen dieser Diplomarbeit wird von uns eine Web-Applikation entwickelt, welche spielerisch die
+        Projektmanagementmethode Scrum vermitteln soll. Lernende sollen in Kleingruppen Städte aus Papier bauen können
+        und währenddessen Einblicke in die verschiedenen Phasen eines Scrum-Projekts bekommen. Dabei werden sie durch
+        vorgefertigte Bastelvorlagen und das Anleiten der Website im Lernprozess unterstützt.
+      </p>
+      <p class="mt-36 font-bold text-sc-orange text-2xl mb-6">
+        Unsere Ziele
+      </p>
+      <ul class="list-disc list-inside text-sm leading-7 mx-8">
+        <li>Lehrkraftprofile inkl. Registrierung & Login</li>
+        <li>Einfache Raumübersicht für die Lehrkraft</li>
+        <li>Gesamter Spielablauf
+          <ul class="list-disc list-inside ml-4">
+            <li>Backlog mit User Storys</li>
+            <li>Sprint-Plannings</li>
+            <li>Bauphase mit Timer</li>
+            <li>Review-Meeting inkl. Velocity</li>
+          </ul>
+        </li>
+        <li>Bastelvorlagen zum Downloaden</li>
+        <li>Gezieltes Marketing</li>
+      </ul>
+      <p class="mt-36 font-bold text-sc-orange text-2xl">
+        Das Team
+      </p>
+      <img src="assets/img/Team/felix.png" class="w-40 mt-4 mb-1.5" alt="Ein Foto des Teammitglieds Felix">
+      <p class="text-sm">Scrum Master & Developer</p>
+      <img src="assets/img/Team/lisa.png" class="w-40 mt-4 mb-1.5" alt="Ein Foto des Teammitglieds Lisa">
+      <p class="text-sm">Product Owner & Developer</p>
+      <img src="assets/img/Team/marco.png" class="w-40 mt-4 mb-1.5" alt="Ein Foto des Teammitglieds Marco">
+      <p class="text-sm">Developer</p>
+      <img src="assets/img/Team/sophie.png" class="w-40 mt-4 mb-1.5" alt="Ein Foto des Teammitglieds Sophie">
+      <p class="text-sm">Head of Design</p>
+    </main>
+    <footer class="flex flex-row text-xs">
+      <p class="ml-2 mr-10">©2024, ScrumpliCity</p>
+      <NuxtLink class="underline">Impressum</NuxtLink>
+    </footer>
   </div>
 </template>
+<script setup lang="ts">
+</script>
