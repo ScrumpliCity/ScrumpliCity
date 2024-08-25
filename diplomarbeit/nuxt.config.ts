526652f7ee4931751215bff09ed0fd9e4871377a
dasechtelima <0198@htl.rennweg.at>
2024-08-25 13:57:16 +0200

    added temporary favicon

--- diplomarbeit/nuxt.config.ts
+++ diplomarbeit/nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true },
-  css: ['~/assets/css/main.css'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
+    devtools: {enabled: true},
+    css: ['~/assets/css/main.css'],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
     },
-  },
+    app: {
+        head: {
+            link: [
+                {rel: 'icon', type: 'image/png', href: '/favicon.png'}
+            ]
+        }
+    }
 })
