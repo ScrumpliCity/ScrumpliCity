42ed3f5d06cfcc0d6d3db6516b59793dccd759d8
dasechtelima <0198@htl.rennweg.at>
2024-08-25 14:33:39 +0200

    fixed scrolling position and header for desktops

--- diplomarbeit/app.vue
+++ diplomarbeit/app.vue
@@ -1,19 +1,27 @@
 <template>
   <div class="bg-sc-white flex flex-col scroll-smooth">
-    <header class="text-xs flex items-end w-screen h-12 mb-14 bg-sc-white justify-between underline px-24 fixed pb-2.5">
-      <a href="#projektbeschreibung">Projekt</a>
-      <a href="#ziele">Ziele</a>
-      <a href="#team">Team</a>
+    <header
+        class="flex items-center text-xs sm:text-xl w-screen flex-row h-12 sm:h-28 bg-sc-white text-center justify-center sm:justify-between sm:px-9 fixed pb-2.5">
+      <a href="/">
+        <img src="assets/img/schriftlogo.svg" class="w-[300px] hidden sm:block"
+             alt="Schriftlogo des Projekts ScrumpliCity, es zeigt den Schriftzug ScrumpliCity mit einer Illustration von 3 Häusern welche von einem Kreis, welcher an Scrum erinnert, umkreist sind">
+      </a>
+      <div class="flex items-end flex-row sm:justify-end underline">
+        <a href="#projektbeschreibung">Projekt</a>
+        <a class="mx-16" href="#ziele">Ziele</a>
+        <a href="#team">Team</a>
+      </div>
     </header>
     <main
-        class="flex flex-col items-center mt-28 bg-[url('/assets/img/bg-houses.svg')] bg-no-repeat bg-[center_top_24rem] sm:bg-[url('/assets/img/bg-houses-big.svg')] sm:bg-[center_top_10rem]">
-      <h1 class="text-3xl font-logo sm:text-6xl">
+        class="flex flex-col items-center mt-16 sm:mt-40 bg-[url('/assets/img/bg-houses.svg')] bg-no-repeat bg-[center_top_24rem] sm:bg-[url('/assets/img/bg-houses-big.svg')] sm:bg-[center_top_10rem]">
+      <h1 class="scroll-mt-12 sm:scroll-mt-28 text-3xl font-logo sm:text-6xl" id="/">
         Willkommen bei
       </h1>
       <img src="assets/img/schriftlogo.svg" class="mx-12 my-2 sm:h-32"
            alt="Schriftlogo des Projekts ScrumpliCity, es zeigt den Schriftzug ScrumpliCity mit einer Illustration von 3 Häusern welche von einem Kreis, welcher an Scrum erinnert, umkreist sind">
 
-      <p class="text-sc-orange mt-12 sm:mt-44 text-2xl sm:text-4xl mb-6 sm:mb-7 font-heading" id="projektbeschreibung">
+      <p class="scroll-mt-12 sm:scroll-mt-28 text-sc-orange mt-12 sm:mt-44 text-2xl sm:text-4xl mb-6 sm:mb-7 font-heading"
+         id="projektbeschreibung">
         Unser Projekt
       </p>
       <p class="text-justify text-sm sm:text-xl leading-6 sm:leading-9 mx-8 sm:mx-[497px]">
@@ -24,7 +32,8 @@
         durch
         vorgefertigte Bastelvorlagen und das Anleiten der Website im Lernprozess unterstützt.
       </p>
-      <p class="mt-28 sm:mt-32 text-sc-orange text-2xl sm:text-4xl mb-6 sm:mb-7 font-heading" id="ziele">
+      <p class="scroll-mt-12 sm:scroll-mt-28 mt-28 sm:mt-32 text-sc-orange text-2xl sm:text-4xl mb-6 sm:mb-7 font-heading"
+         id="ziele">
         Unsere Ziele
       </p>
       <ul class="list-disc list-inside text-sm sm:text-xl leading-7 sm:leading-10 mx-8">
@@ -41,7 +50,7 @@
         <li>Bastelvorlagen zum Downloaden</li>
         <li>Gezieltes Marketing</li>
       </ul>
-      <p class="mt-28 sm:mt-32 text-sc-orange text-2xl sm:text-4xl font-heading" id="team">
+      <p class="scroll-mt-12 sm:scroll-mt-0 mt-28 sm:mt-32 text-sc-orange text-2xl sm:text-4xl font-heading" id="team">
         Das Team
       </p>
       <div class="sm:flex sm:flex-row sm:mt-4 text-sm sm:text-xl text-center">
