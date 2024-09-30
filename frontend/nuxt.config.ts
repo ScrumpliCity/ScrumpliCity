b746ac2d902c26e71d8e9b7a978e5ae16b1798f0
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2024-09-30 15:47:23 +0200

    Fixed I18n config and rearranged files

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,4 +3,14 @@ export default defineNuxtConfig({
   compatibilityDate: "2024-04-03",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  i18n: {
+    defaultLocale: "de",
+    locales: [
+      { code: "de", file: "de.json" },
+      { code: "en", file: "en.json" },
+    ],
+    lazy: true,
+    langDir: "locales/",
+    vueI18n: "./i18n.config.ts",
+  },
 });
---
+++ frontend/pages/index.vue
@@ -0,0 +1,14 @@
+<script setup>
+const { locale, setLocale } = useI18n()
+</script>
+
+<template>
+    <div>
+        <div>
+            <button @click="setLocale('de')">de</button>
+            <br>
+            <button @click="setLocale('en')">en</button>
+            <p>{{ $t('welcome') }}</p>
+        </div>
+    </div>
+</template>
