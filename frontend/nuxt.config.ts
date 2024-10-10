f0ca14f5411e8d6198abe2472dd31ee00b9646dd
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-10-10 21:54:04 +0200

    update compatability date\n\nwe dont rely on any build provider currently so why not update

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,5 +1,5 @@
 export default defineNuxtConfig({
-  compatibilityDate: "2024-04-03",
+  compatibilityDate: "2024-10-10",
   devtools: { enabled: true },
   modules: ["@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
   i18n: {
