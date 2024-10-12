3ea17433faa963c4aa6560bb2dacaca93f540d9c
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-10-12 22:52:48 +0200

    add sanctum login

---
+++ frontend/pages/login.vue
@@ -0,0 +1,23 @@
+<script setup>
+definePageMeta({
+    middleware: ['sanctum:guest'],
+});
+
+const { login } = useSanctumAuth();
+
+const router = useRouter();
+
+const userCredentials = {
+    email: 'test@example.com',
+    password: 'password',
+};
+
+async function doLogin() {
+    await login(userCredentials);
+    router.push('/logged_in');
+
+}
+</script>
+<template>
+<button @click="doLogin">log me in</button>
+</template>
