65c7e98e8b8a2c237d670740ed8ac9f77fa6feb8
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-10-18 17:55:46 +0200

    add command to generate api tokens for testing

--- backend/routes/console.php
+++ backend/routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Models\User;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+
+Artisan::command('get-api-token', function () {
+    $user = User::first();
+    $user->tokens()->delete();
+    $this->comment("Generating your API token for: " . $user->email . " id: " . $user->id);
+    $token = $user->createToken('API Token')->plainTextToken;
+    $this->comment('Your API token is: ' . $token);
+})->purpose('Get API token for the current user');
