f1bfb485bb3b729a8a7b3839c49c458fe9d31c2b
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-10-18 18:37:48 +0200

    format code

--- frontend/pages/logged_in.vue
+++ frontend/pages/logged_in.vue
@@ -8,13 +8,11 @@ const user = useSanctumUser();
 // fetch sth
 const client = useSanctumClient();
 
-const { data } = await useAsyncData('rooms', () => client('/api/rooms'));
-
-
+const { data } = await useAsyncData("rooms", () => client("/api/rooms"));
 </script>
 <template>
-<p>logged in! User: {{ user }}</p>
-    <ul class="list-disc pl-8">
-        <li v-for="room in data">{{ room.name }}</li>
-    </ul>
+  <p>logged in! User: {{ user }}</p>
+  <ul class="list-disc pl-8">
+    <li v-for="room in data">{{ room.name }}</li>
+  </ul>
 </template>
