ab70ff959a79d7f3d043c59e142922f3aa57bbe8
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-10-29 18:14:20 +0100

    SC-13: add creation button

--- frontend/pages/rooms.vue
+++ frontend/pages/rooms.vue
@@ -1,9 +1,11 @@
 <script lang="ts" setup>
-import type { Room } from "~/types/api";
-
 definePageMeta({
   middleware: ["sanctum:auth"],
 });
+
+import type { Room } from "~/types/api";
+
+const localePath = useLocalePath();
 
 const client = useSanctumClient();
 
@@ -69,7 +71,7 @@ const completedRooms = computed(() => {
 
 <template>
   <AppHeader />
-  <div class="flex flex-col gap-6 pl-20 pr-14 pt-14">
+  <div class="relative flex flex-col gap-6 pl-20 pr-14 pt-14">
     <RoomDivider
       iconClass="text-sc-green"
       :text="$t('rooms.started')"
@@ -109,7 +111,37 @@ const completedRooms = computed(() => {
       >
       </RoomCard>
     </div>
-    {{ data }}
+    <div class="fixed bottom-16 right-16 z-10 overflow-hidden pt-44">
+      <div class="relative">
+        <UButton
+          class="peer relative z-10 h-20 w-44 items-center justify-center text-4xl font-bold transition-colors hover:bg-sc-orange-700"
+          size="xl"
+          :to="localePath('rooms-create')"
+        >
+          {{ $t("rooms.new") }}
+        </UButton>
+
+        <svg
+          class="absolute left-1/2 top-0 size-40 -translate-x-1/2 transition-transform duration-300 peer-hover:-translate-y-full"
+          width="615"
+          height="628"
+          viewBox="0 0 615 628"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g opacity="0.25">
+            <path
+              d="M146.908 307.143V0H427.957V216.539H364.126V629H260.643V307.724L146.908 307.24V307.143Z"
+              fill="#CE6326"
+            />
+            <path d="M0 307.144H260.642V629H0" fill="#8CDBA4" />
+            <path
+              d="M615 216.442H364.029V628.903C364.029 628.903 440.626 628.903 615 628.903"
+              fill="#E0AD49"
+            />
+          </g>
+        </svg>
+      </div>
+    </div>
   </div>
 
   <!-- Slot for room creation popup -->
