debd96b4ee5422d705a05e043dffcadc1a0c2bb6
dasechtelima <0198@htl.rennweg.at>
2024-11-01 14:23:23 +0100

    prototype of Infobox for SC-39 Rollenauswahl

---
+++ frontend/components/Infobox.vue
@@ -0,0 +1,32 @@
+<script>
+const title = "";
+const description = "";
+const actions = [];
+const timeout = 0;
+
+function addInfoBox(title, description, actions, timeout) {
+  this.title = title;
+  this.description = description;
+  this.actions = actions;
+  this.timeout = timeout;
+  useToast();
+  console.log("InfoBox added"); //wird ausgeführt, aber die Infobox wird noch nicht angezeigt
+}
+
+export default {
+  addInfoBox,
+};
+</script>
+
+<template>
+  <UNotification
+    icon="ic:outline-info"
+    color="blue"
+    actions="{{ actions }}"
+    title="{{ title }}"
+    description="{{ description }}"
+    timeout="{{ timeout }}"
+  />
+</template>
+
+<style scoped></style>
--- frontend/pages/add_team_members.vue
+++ frontend/pages/add_team_members.vue
@@ -1,150 +1,199 @@
 <script setup xmlns="http://www.w3.org/1999/html">
-const {locale, setLocale} = useI18n();
+import { ref } from "vue";
+import Infobox from "~/components/Infobox.vue";
+
+const { locale, setLocale } = useI18n();
 const localeRoute = useLocaleRoute();
+const { t } = useI18n();
 
 function switchLocale() {
   setLocale(locale.value === "de" ? "en" : "de");
 }
 
 //@TODO: get name from backend and store members
-const roomCode = ref('TestCode');
-const teamName = ref('Testteam');
+const roomCode = ref("TestCode");
+const teamName = ref("Testteam");
 
-const teamMembers = ref([{
-  id: 0,
-  name: 'Felix',
-  role: 'Developer',
-},
+const teamMembers = ref([
+  {
+    id: 0,
+    name: "Felix",
+    role: "Developer",
+  },
   {
     id: 1,
-    name: 'Lisa-Marie Hörmann',
-    role: 'Developer',
+    name: "Lisa-Marie Hörmann",
+    role: "Developer",
   },
   {
     id: 2,
-    name: 'Sophie',
-    role: 'Developer',
+    name: "Sophie",
+    role: "Developer",
   },
   {
     id: 3,
-    name: 'Marco',
-    role: 'Developer',
-  }
+    name: "Marco",
+    role: "Developer",
+  },
 ]);
 
 const roles = [
   {
-    label: 'Developer',
+    label: "Developer",
   },
-  {label: 'Scrum Master'},
-  {label: 'Product Owner'},
-]
+  { label: "Scrum Master" },
+  { label: "Product Owner" },
+];
 
-const newMember = ref('');
+const newMember = ref("");
 
 function deleteMember(memberId) {
-  teamMembers.value = teamMembers.value.filter(member => member.id !== memberId);
+  teamMembers.value = teamMembers.value.filter(
+    (member) => member.id !== memberId,
+  );
 }
 
 function addMember() {
   teamMembers.value.push({
     id: teamMembers.value.length,
     name: newMember.value,
-    role: 'Developer',
+    role: "Developer",
   });
-  newMember.value = '';
+  newMember.value = "";
+  Infobox.addInfoBox(
+    t("infobox.startup.title"),
+    t("infobox.startup.description"),
+    [
+      {
+        label: t("infobox.startup.button"),
+        color: "blue",
+      },
+    ],
+    "0",
+  );
 }
 
 const isRoleTaken = (role) => {
-  if (role !== 'Developer') {
-    return teamMembers.value.some(member => member.role === role);
+  if (role !== "Developer") {
+    return teamMembers.value.some((member) => member.role === role);
   }
 };
 
 function updateRole(memberId, newRole) {
-  const member = teamMembers.value.find(member => member.id === memberId);
+  const member = teamMembers.value.find((member) => member.id === memberId);
   if (member) {
     member.role = newRole;
   }
 }
 
 function isReady() {
-  const scrumMasters = teamMembers.value.filter(member => member.role === 'Scrum Master').length;
-  const productOwners = teamMembers.value.filter(member => member.role === 'Product Owner').length;
+  const scrumMasters = teamMembers.value.filter(
+    (member) => member.role === "Scrum Master",
+  ).length;
+  const productOwners = teamMembers.value.filter(
+    (member) => member.role === "Product Owner",
+  ).length;
   return scrumMasters === 1 && productOwners >= 1;
 }
-
 </script>
 <template>
   <div
-      class="flex flex-col w-full h-screen bg-[url('/assets/svg/join-room/add_team_members.svg')] bg-contain bg-no-repeat bg-bottom">
-    <div class="flex w-full place-content-end pt-11 pr-11">
+    class="flex h-screen w-full flex-col bg-[url('/assets/svg/join-room/add_team_members.svg')] bg-contain bg-bottom bg-no-repeat"
+  >
+    <div class="flex w-full place-content-end pr-11 pt-11">
       <UButton
-          @click="switchLocale()"
-          :padded="false"
-          variant="ghost"
-          class="hover:bg-transparent"
+        @click="switchLocale()"
+        :padded="false"
+        variant="ghost"
+        class="hover:bg-transparent"
       >
         <template #leading>
           <LazySvgI18nDe
-              v-if="locale === 'de'"
-              :font-controlled="false"
-              class="size-14"
-              filled
+            v-if="locale === 'de'"
+            :font-controlled="false"
+            class="size-14"
+            filled
           />
           <LazySvgI18nEn
-              v-else
-              :font-controlled="false"
-              class="size-14"
-              filled
+            v-else
+            :font-controlled="false"
+            class="size-14"
+            filled
           />
         </template>
       </UButton>
     </div>
     <div class="ml-[10%] mr-[40%]">
-      <p class="text-3xl font-heading font-medium ml-[3%]"> {{ roomCode }}</p>
-      <h1 class="font-heading text-6xl font-bold text-sc-orange mb-[5%] ml-[3%]">
-        {{ teamName }} </h1>
+      <p class="ml-[3%] font-heading text-3xl font-medium">{{ roomCode }}</p>
+      <h1
+        class="mb-[5%] ml-[3%] font-heading text-6xl font-bold text-sc-orange"
+      >
+        {{ teamName }}
+      </h1>
       <div
-          class="flex flex-col flex-grow-0 max-h-[43vh] justify-center border-2 border-sc-black-400 rounded-lg p-7 mb-4 bg-sc-white drop-shadow-sc-48025">
+        class="mb-4 flex max-h-[43vh] flex-grow-0 flex-col justify-center rounded-lg border-2 border-sc-black-400 bg-sc-white p-7 drop-shadow-sc-48025"
+      >
         <div class="flex overflow-y-scroll">
           <table class="w-full">
             <tbody>
-            <tr v-for="member in teamMembers" :key="member.id" class="border-b-2 border-sc-black flex justify-between">
-              <td class="text-3xl p-2 flex-grow">{{ member.name }}</td>
-              <td class="flex items-center">
-                <select
-                    class="rounded-md p-1 mr-4"
+              <tr
+                v-for="member in teamMembers"
+                :key="member.id"
+                class="flex justify-between border-b-2 border-sc-black"
+              >
+                <td class="flex-grow p-2 text-3xl">{{ member.name }}</td>
+                <td class="flex items-center">
+                  <select
+                    class="mr-4 rounded-md p-1"
                     v-model="member.role"
-                    @change="updateRole(member.id, $event.target.value)">
-                  <option v-for="role in roles" :key="role.label" :value="role.label"
-                          :disabled="isRoleTaken(role.label)">
-                    {{ role.label }}
-                  </option>
-                </select>
-                <button @click="deleteMember(member.id)">
-                  <UIcon name="mdi:trash-can-outline" class="text-sc-orange p-4"/>
-                </button>
-              </td>
-            </tr>
+                    @change="updateRole(member.id, $event.target.value)"
+                  >
+                    <option
+                      v-for="role in roles"
+                      :key="role.label"
+                      :value="role.label"
+                      :disabled="isRoleTaken(role.label)"
+                    >
+                      {{ role.label }}
+                    </option>
+                  </select>
+                  <UButton
+                    @click="deleteMember(member.id)"
+                    icon="mdi:trash-can-outline"
+                    class="size-10 justify-center hover:bg-sc-black-100"
+                    :padded="false"
+                    variant="ghost"
+                    color="sc-orange"
+                    :ui="{
+                      rounded: 'rounded-full',
+                      icon: { size: { sm: 'size-6' } },
+                    }"
+                  >
+                  </UButton>
+                </td>
+              </tr>
             </tbody>
           </table>
         </div>
         <!-- @submit.prevent is a workaround so the page isn't reloaded till the backend exists-->
         <form @submit.prevent>
           <input
-              class="w-full rounded-lg bg-sc-yellow/70 drop-shadow-sc-48025 text-3xl font-bold p-1 mt-7 text-center placeholder:text-sc-black focus:placeholder:text-transparent"
-              :placeholder="$t('join-room.add-team-member')"
-              v-model="newMember"
+            class="mt-7 w-full rounded-lg bg-sc-yellow/70 p-1 text-center text-3xl font-bold drop-shadow-sc-48025 placeholder:text-sc-black focus:placeholder:text-transparent"
+            :placeholder="$t('join-room.add-team-member')"
+            v-model="newMember"
           />
-          <button @click="addMember()"/>
+          <button @click="addMember()" />
         </form>
       </div>
-      <div class="w-full flex justify-end">
+      <div class="flex w-full justify-end">
         <button
-            :disabled="!isReady()"
-            class="w-72 rounded-lg py-6 text-center text-4xl font-bold drop-shadow-sc-48025"
-            :class="isReady() ? 'bg-sc-green text-sc-black hover:bg-sc-green-400' : 'text-sc-white bg-sc-black-400'"
+          :disabled="!isReady()"
+          class="w-72 rounded-lg py-6 text-center text-4xl font-bold drop-shadow-sc-48025"
+          :class="
+            isReady()
+              ? 'bg-sc-green text-sc-black hover:bg-sc-green-400'
+              : 'bg-sc-black-400 text-sc-white'
+          "
         >
           {{ $t("join-room.add-members-ready") }}
         </button>
