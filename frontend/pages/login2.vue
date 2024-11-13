df7a0164ed9202389855a968d51f4dbfd88435eb
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-11-13 14:41:18 +0000

    SC-5: add login frontend

--- frontend/pages/login2.vue
+++ frontend/pages/login2.vue
@@ -1,30 +1,38 @@
 <script setup lang="ts">
 definePageMeta({
   layout: "login",
+  middleware: ["sanctum:guest"],
 });
 
 const client = useSanctumClient();
+const auth = useSanctumAuth();
+const localeRoute = useLocaleRoute();
+const { t } = useI18n();
 
 const popupIsOpen = ref(false);
 
 const checkOpenWindowIntervalId: Ref<number | undefined> = ref();
 
 async function loginWithMicrosoft() {
-  popupIsOpen.value = true;
+  try {
+    popupIsOpen.value = true;
 
-  // get redirect url from backend
-  const { url } = await client("/oauth/microsoft");
+    // get redirect url from backend
+    const { url } = await client("/oauth/microsoft");
 
-  // open popup
-  const popup = window.open(url, "_blank", "width=600,height=600");
+    // open popup
+    const popup = window.open(url, "_blank", "width=600,height=600");
 
-  // check periodically if popup is closed to enable button again
-  checkOpenWindowIntervalId.value = setInterval(() => {
-    if (!popup || popup.closed) {
-      clearInterval(checkOpenWindowIntervalId.value);
-      popupIsOpen.value = false;
-    }
-  }, 50) as unknown as number;
+    // check periodically if popup is closed to enable button again
+    checkOpenWindowIntervalId.value = setInterval(() => {
+      if (!popup || popup.closed) {
+        clearInterval(checkOpenWindowIntervalId.value);
+        popupIsOpen.value = false;
+      }
+    }, 50) as unknown as number;
+  } catch {
+    showErrorMessage();
+  }
 }
 
 function clearCheckOpenWindowInterval() {
@@ -34,12 +42,32 @@ function clearCheckOpenWindowInterval() {
 const isLoggingIn = ref(false);
 
 async function handleCallback(event: MessageEvent) {
-  clearCheckOpenWindowInterval();
+  try {
+    clearCheckOpenWindowInterval();
+    popupIsOpen.value = false;
+    isLoggingIn.value = true;
+    const { code } = event.data;
+    const response = await client("/oauth/microsoft/callback", {
+      params: {
+        code,
+      },
+    });
+    await auth.refreshIdentity();
+    await navigateTo(localeRoute({ path: "/rooms" }));
+  } catch {
+    showErrorMessage();
+  }
+}
+
+function showErrorMessage() {
+  isLoggingIn.value = false;
   popupIsOpen.value = false;
-  isLoggingIn.value = true;
-  const { code } = event.data;
-  console.log(code);
-  console.log("🎉");
+  clearCheckOpenWindowInterval();
+
+  useToast().add({
+    title: t("login.error"),
+    icon: "material-symbols:error-outline",
+  });
 }
 
 onMounted(() => {
