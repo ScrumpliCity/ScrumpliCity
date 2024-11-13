cbc932735a49a9f4cd36ed67c537be8675907283
Felix Wollmann <50966625+felixwollmann@users.noreply.github.com>
2024-11-13 16:31:28 +0000

    SC-5: clean up remainders of email login

--- backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ backend/app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -13,37 +13,8 @@ use Illuminate\Http\JsonResponse;
 
 class AuthenticatedSessionController extends Controller
 {
-    /**
-     * Handle an incoming authentication request.
-     */
-    public function store(LoginRequest $request): Response
-    {
-        $request->authenticate();
-
-        $request->session()->regenerate();
-
-        return response()->noContent();
-    }
-
-    /**
-     * Destroy an authenticated session.
-     */
-    public function destroy(Request $request): Response
-    {
-        Auth::guard('web')->logout();
-
-        $request->session()->invalidate();
-
-        $request->session()->regenerateToken();
-
-        return response()->noContent();
-    }
-
     public function getMicrosoftURL(): JsonResponse
     {
-
-
-        // Socialite::driver('microsoft')->redirect();
         return response()->json([
             'url' => Socialite::driver('microsoft')->stateless()->redirect()->getTargetUrl()
         ]);
