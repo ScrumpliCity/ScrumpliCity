d9ff2704045270a42546ea7d5e94a9d0899d6faf
dasechtelima <0198@htl.rennweg.at>
2024-11-16 15:50:42 +0100

    SC-39: add fold animation to infobox

--- frontend/components/Infobox.vue
+++ frontend/components/Infobox.vue
@@ -12,27 +12,25 @@ const closeInfobox = () => {
 
 <template>
   <div>
-    <div
-      v-if="infoboxIsOpen"
-      class="fixed bottom-10 right-10 h-fit max-w-[23vw] rounded-xl border-sc-black-200 bg-sc-blue-light p-4 drop-shadow-sc-shadow"
-    >
-      <div class="flex items-center">
-        <UIcon
-          name="ic:outline-info"
-          class="mr-2 max-h-[76px] bg-sc-blue"
-          size="5vw"
-        />
-        <slot name="title" />
+    <transition name="infobox">
+      <div
+        v-if="infoboxIsOpen"
+        class="fixed bottom-10 right-10 h-fit max-w-[23vw] rounded-xl border-sc-black-200 bg-sc-blue-light p-4 drop-shadow-sc-shadow"
+      >
+        <div class="flex items-center">
+          <UIcon name="ic:outline-info" class="mr-2 bg-sc-blue" size="5vw" />
+          <slot name="title" />
+        </div>
+        <slot name="content" />
+        <div class="flex justify-end">
+          <slot name="button" :close="closeInfobox" />
+        </div>
+        <slot name="progressBar" />
       </div>
-      <slot name="content" />
-      <div class="flex justify-end">
-        <slot name="button" :close="closeInfobox" />
-      </div>
-      <slot name="progressBar" />
-    </div>
-    <div v-else>
+    </transition>
+    <div v-if="!infoboxIsOpen">
       <UButton
-        class="fixed bottom-10 right-10 rounded-full bg-sc-blue-light p-0 drop-shadow-sc-shadow hover:bg-sc-blue"
+        class="fixed bottom-5 right-5 rounded-full bg-sc-blue-light p-0 drop-shadow-sc-shadow hover:bg-sc-blue"
         @click="infoboxIsOpen = true"
       >
         <UIcon
@@ -44,3 +42,25 @@ const closeInfobox = () => {
     </div>
   </div>
 </template>
+<style scoped>
+.infobox-enter-active,
+.infobox-leave-active {
+  transition:
+    transform 0.5s,
+    opacity 0.5s;
+}
+
+.infobox-enter-from,
+.infobox-leave-to {
+  transform: scale(0);
+  opacity: 0.5;
+  transform-origin: bottom right;
+}
+
+.infobox-enter-to,
+.infobox-leave-from {
+  transform: scale(1);
+  opacity: 1;
+  transform-origin: bottom right;
+}
+</style>
