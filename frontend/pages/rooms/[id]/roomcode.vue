845365bd62a46a1c36fb8e0a025b83f4dfe656dd
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2024-11-20 12:09:09 +0100

    SC-170 Replaced let with const

--- frontend/pages/rooms/[id]/roomcode.vue
+++ frontend/pages/rooms/[id]/roomcode.vue
@@ -12,7 +12,7 @@ const { data: roomcodeData } = await useAsyncData("roomcode", () =>
   }),
 );
 
-let roomcode = ref(roomcodeData.value.roomcode || "");
+const roomcode = ref(roomcodeData.value.roomcode || "");
 </script>
 <template>
   <div class="flex h-full flex-col items-center text-center">
