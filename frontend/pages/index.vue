18e0b0e1bc3ab10be420d9d6254b09994ebfc003
dasechtelima <0198@htl.rennweg.at>
2024-12-28 15:47:43 +0100

    SC-2: updating responsivness for up to 1536px width

--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -47,17 +47,17 @@
   </div>
   <div class="relative flex flex-col">
     <HomeHeader />
-    <div class="mt-28 flex flex-col lg:mt-20">
+    <div class="mt-28 flex flex-col lg:mt-20 xl:mt-[8vw]">
       <div
-        class="m-10 h-56 min-[580px]:mr-32 sm:ml-14 lg:ml-20 xl:ml-28 xl:mt-32"
+        class="m-10 h-56 min-[580px]:mr-32 sm:ml-14 lg:ml-20 xl:ml-28 min-[1330px]:mr-[38vw]"
       >
         <SvgMainLogo
-          class="w-[70vw] sm:w-[300px] md:w-[40vw] md:max-w-[450px]"
+          class="w-[70vw] sm:w-[300px] md:w-[40vw] md:max-w-[450px] lg:max-w-[520px] xl:max-w-[590px]"
           :fontControlled="false"
           filled
         />
         <p
-          class="my-3 max-w-[835px] bg-sc-white/70 text-left text-sm xs:text-base min-[460px]:text-lg lg:max-w-[60vw] min-[1250px]:text-xl xl:text-2xl"
+          class="my-3 max-w-[835px] bg-sc-white/70 text-left text-sm xs:text-base min-[460px]:text-lg lg:max-w-[60vw] min-[1190px]:text-xl min-[1330px]:text-2xl"
         >
           <span v-html="$t('homepage.project_description')"></span>
           <b>{{ $t("homepage.build_your_scrum_knowledge") }}</b>
@@ -66,7 +66,7 @@
           <UButton
             :disabled="true"
             style="opacity: 100"
-            class="hidden drop-shadow-sc-shadow hover:bg-orange-700 lg:block lg:px-5 lg:py-2 lg:text-base lg:font-bold"
+            class="hidden drop-shadow-sc-shadow hover:bg-orange-700 lg:block lg:px-5 lg:py-2 lg:text-base lg:font-bold xl:text-xl"
             >{{ $t("homepage.get_started") }}</UButton
           >
           <UButton
@@ -78,10 +78,10 @@
         >
       </div>
       <div
-        class="mt-14 flex min-h-[710px] w-screen flex-col items-center xs:mt-[25vw] min-[394px]:min-h-[705px] min-[400px]:min-h-[782px] min-[415px]:mt-[33vw] min-[455px]:mt-[40vw] min-[480px]:mt-[20vw] min-[480px]:min-h-[820px] min-[520px]:min-h-[872px] min-[570px]:mt-[33vw] sm:mt-[10vw] sm:min-h-[650px] min-[660px]:mt-[15vw] min-[684px]:mt-[10vw] min-[720px]:mt-[5vw] min-[855px]:min-h-[350px] lg:mt-[15vw]"
+        class="mt-14 flex min-h-[710px] w-screen flex-col items-center xs:mt-[25vw] min-[394px]:min-h-[705px] min-[400px]:min-h-[782px] min-[415px]:mt-[33vw] min-[455px]:mt-[40vw] min-[480px]:mt-[20vw] min-[480px]:min-h-[820px] min-[520px]:min-h-[872px] min-[570px]:mt-[33vw] sm:mt-[10vw] sm:min-h-[650px] min-[660px]:mt-[15vw] min-[684px]:mt-[10vw] min-[720px]:mt-[5vw] min-[855px]:min-h-[350px] lg:mt-[15vw] xl:mt-[12vw] min-[1400px]:mt-[15vw]"
       >
         <h2
-          class="mb-5 bg-sc-white/70 font-heading text-xl font-bold text-sc-orange min-[480px]:text-2xl min-[720px]:mb-8 min-[720px]:text-3xl min-[1250px]:text-4xl xl:mb-16 xl:text-6xl"
+          class="mb-5 bg-sc-white/70 font-heading text-xl font-bold text-sc-orange min-[480px]:text-2xl min-[720px]:mb-8 min-[720px]:text-3xl lg:text-4xl xl:mb-14 xl:text-5xl"
         >
           {{ $t("homepage.build_your_scrum_knowledge") }}
         </h2>
@@ -89,57 +89,63 @@
           class="flex flex-col justify-center gap-6 min-[490px]:gap-[6vw] sm:mx-10 sm:flex-row sm:flex-wrap min-[855px]:gap-[4vw]"
         >
           <div
-            class="w-52 max-w-[275px] rounded-lg border border-sc-black bg-sc-black-50 p-4 drop-shadow-sc-shadow min-[480px]:w-[55vw] sm:w-[224px] lg:w-[260px] xl:w-80"
+            class="w-52 max-w-[275px] rounded-lg border border-sc-black bg-sc-black-50 p-4 drop-shadow-sc-shadow min-[480px]:w-[55vw] sm:w-[224px] lg:w-[260px] min-[1400px]:w-80 min-[1400px]:max-w-none"
           >
             <h3
-              class="font-heading text-xl min-[520px]:text-2xl xl:mb-3 xl:text-3xl"
+              class="font-heading text-xl min-[520px]:text-2xl xl:mb-3 xl:text-3xl min-[1400px]:text-4xl"
             >
               {{ $t("homepage.features.learn_scrum.title") }}
             </h3>
-            <p class="text-xs min-[400px]:text-sm min-[480px]:text-base">
+            <p
+              class="text-xs min-[400px]:text-sm min-[480px]:text-base min-[860px]:mb-6 lg:mb-1 lg:text-lg min-[1400px]:text-xl"
+            >
               {{ $t("homepage.features.learn_scrum.description") }}
             </p>
             <div class="flex justify-end">
               <SvgScrumProcessIllustration
-                class="h-16 min-[520px]:h-20 sm:h-16 xl:h-28"
+                class="h-16 min-[520px]:h-20 sm:h-16 xl:h-24"
                 :fontControlled="false"
                 filled
               />
             </div>
           </div>
           <div
-            class="w-52 max-w-[275px] rounded-lg border border-sc-black bg-sc-black-50 p-4 drop-shadow-sc-shadow min-[480px]:w-[55vw] sm:w-[224px] lg:w-[260px] xl:w-80"
+            class="w-52 max-w-[275px] rounded-lg border border-sc-black bg-sc-black-50 p-4 drop-shadow-sc-shadow min-[480px]:w-[55vw] sm:w-[224px] lg:w-[260px] min-[1400px]:w-80 min-[1400px]:max-w-none"
           >
             <h3
-              class="font-heading text-xl min-[520px]:text-2xl xl:mb-3 xl:text-3xl"
+              class="font-heading text-xl min-[520px]:text-2xl xl:mb-3 xl:text-3xl min-[1400px]:text-4xl"
             >
               {{ $t("homepage.features.craft_your_city.title") }}
             </h3>
-            <p class="mb-1 text-xs min-[400px]:text-sm min-[480px]:text-base">
+            <p
+              class="mb-1 text-xs min-[400px]:text-sm min-[480px]:text-base min-[860px]:mb-6 lg:mb-7 lg:text-lg min-[1400px]:text-xl"
+            >
               {{ $t("homepage.features.craft_your_city.description") }}
             </p>
             <div class="flex justify-end">
               <SvgCraftingTemplateIllustration
-                class="h-16 min-[520px]:h-20 sm:h-16 xl:h-28"
+                class="h-16 min-[520px]:h-20 sm:h-16 xl:h-24"
                 :fontControlled="false"
                 filled
               />
             </div>
           </div>
           <div
-            class="w-52 max-w-[275px] rounded-lg border border-sc-black bg-sc-black-50 p-4 drop-shadow-sc-shadow min-[480px]:w-[55vw] sm:w-[224px] lg:w-[260px] xl:mb-6 xl:w-80"
+            class="w-52 max-w-[275px] rounded-lg border border-sc-black bg-sc-black-50 p-4 drop-shadow-sc-shadow min-[480px]:w-[55vw] sm:w-[224px] lg:w-[260px] min-[1400px]:w-80 min-[1400px]:max-w-none"
           >
             <h3
-              class="font-heading text-xl min-[520px]:text-2xl xl:mb-3 xl:text-3xl"
+              class="font-heading text-xl min-[520px]:text-2xl xl:mb-3 xl:text-3xl min-[1400px]:text-4xl"
             >
               {{ $t("homepage.features.solidify_teamwork.title") }}
             </h3>
-            <p class="text-xs min-[400px]:text-sm min-[480px]:text-base">
+            <p
+              class="text-xs min-[400px]:text-sm min-[480px]:text-base lg:text-lg min-[1400px]:text-xl"
+            >
               {{ $t("homepage.features.solidify_teamwork.description") }}
             </p>
             <div class="flex justify-end">
               <SvgTeamworkIllustration
-                class="h-16 min-[520px]:h-20 sm:h-16 xl:h-28"
+                class="h-16 min-[520px]:h-20 sm:h-16 xl:h-24"
                 :fontControlled="false"
                 filled
               />
@@ -149,7 +155,7 @@
       </div>
       <div class="w-screen">
         <div
-          class="mx-[11vw] mt-[15vw] min-h-[370px] max-w-80 min-[377px]:mt-[30vw] min-[394px]:min-h-[420px] min-[400px]:mt-[24vw] min-[425px]:mt-[35vw] min-[440px]:mt-[25vw] min-[450px]:mt-[35vw] min-[470px]:mt-[45vw] min-[480px]:mt-[17vw] min-[480px]:min-h-[508px] min-[484px]:mt-[23vw] min-[504px]:mt-[30vw] min-[516px]:mt-[33vw] min-[540px]:mt-[40vw] min-[620px]:mt-[50vw] sm:mt-[0vw] sm:min-h-[308px] sm:max-w-none min-[650px]:mt-[3vw] min-[675px]:mt-[6vw] min-[695px]:mt-[10vw] min-[730px]:mt-[12vw] min-[740px]:mt-[17vw] md:mt-[5vw] min-[790px]:mt-[10vw] min-[844px]:mt-[15vw] min-[855px]:mt-[2vw] min-[870px]:mt-[6vw] min-[970px]:mt-[12vw] lg:mt-[8vw] min-[1060px]:mt-[11vw]"
+          class="mx-[11vw] mt-[15vw] min-h-[370px] max-w-80 min-[377px]:mt-[30vw] min-[394px]:min-h-[420px] min-[400px]:mt-[24vw] min-[425px]:mt-[35vw] min-[440px]:mt-[25vw] min-[450px]:mt-[35vw] min-[470px]:mt-[45vw] min-[480px]:mt-[17vw] min-[480px]:min-h-[508px] min-[484px]:mt-[23vw] min-[504px]:mt-[30vw] min-[516px]:mt-[33vw] min-[540px]:mt-[40vw] min-[620px]:mt-[50vw] sm:mt-[0vw] sm:min-h-[308px] sm:max-w-none min-[650px]:mt-[3vw] min-[675px]:mt-[6vw] min-[695px]:mt-[10vw] min-[730px]:mt-[12vw] min-[740px]:mt-[17vw] md:mt-[5vw] min-[790px]:mt-[10vw] min-[844px]:mt-[15vw] min-[855px]:mt-[2vw] min-[870px]:mt-[6vw] min-[970px]:mt-[12vw] lg:mt-[8vw] min-[1060px]:mt-[11vw] min-[1190px]:mt-[15vw] xl:mt-[13vw] min-[1400px]:mt-[15vw] min-[1480px]:mt-[20vw]"
         >
           <h3
             class="mb-2 bg-sc-white/70 font-heading text-sm font-bold text-sc-black-700 min-[400px]:text-base min-[480px]:text-lg lg:text-xl xl:text-2xl"
@@ -157,19 +163,19 @@
             {{ $t("homepage.learning_materials") }}
           </h3>
           <h4
-            class="mb-6 bg-sc-white/70 font-heading text-xl font-bold text-sc-orange min-[480px]:text-2xl sm:mb-2 lg:text-4xl xl:text-6xl"
+            class="mb-6 bg-sc-white/70 font-heading text-xl font-bold text-sc-orange min-[480px]:text-2xl sm:mb-2 lg:text-4xl xl:text-5xl"
           >
             {{ $t("homepage.all_you_need_to_know") }}
           </h4>
           <p
-            class="mb-4 bg-sc-white/70 text-sm min-[394px]:text-base min-[480px]:text-lg sm:text-base min-[720px]:text-lg lg:text-xl xl:mb-6 xl:text-2xl"
+            class="mb-4 bg-sc-white/70 text-sm min-[394px]:text-base min-[480px]:text-lg sm:text-base min-[720px]:text-lg lg:text-xl xl:text-2xl"
             v-html="$t('homepage.scrum_guide_description')"
           ></p>
           <UTooltip :text="$t('tooltip.not_available')">
             <UButton
               :disabled="true"
               style="opacity: 100"
-              class="drop-shadow-sc-shadow hover:bg-orange-700 lg:px-5 lg:py-2 lg:text-base lg:font-bold"
+              class="drop-shadow-sc-shadow hover:bg-orange-700 lg:px-5 lg:py-2 lg:text-base lg:font-bold xl:text-xl"
               >{{ $t("homepage.scrum_guide") }}</UButton
             ></UTooltip
           >
@@ -177,7 +183,7 @@
       </div>
       <div class="flex w-screen flex-col items-end sm:px-[5vw]">
         <div
-          class="mx-10 mb-16 mt-20 max-w-[475px] text-right xs:mb-[20vw] xs:mt-[23vw] min-[370px]:mt-[30vw] min-[400px]:mb-[25vw] min-[400px]:mt-[20vw] min-[410px]:mt-[27vw] min-[435px]:mt-[34vw] min-[470px]:mb-[45vw] min-[480px]:mb-[28vw] min-[480px]:mt-[13vw] min-[530px]:mt-[18vw] min-[600px]:mt-[30vw] sm:mb-[9vw] sm:mt-[7vw] min-[720px]:mt-[12vw] md:mt-[10vw] min-[830px]:mb-[12vw] min-[830px]:mt-[15vw] min-[855px]:mt-[12vw] lg:mb-[15vw] min-[1100px]:mt-[15vw]"
+          class="mx-10 mb-16 mt-20 max-w-[475px] text-right xs:mb-[20vw] xs:mt-[23vw] min-[370px]:mt-[30vw] min-[400px]:mb-[25vw] min-[400px]:mt-[20vw] min-[410px]:mt-[27vw] min-[435px]:mt-[34vw] min-[470px]:mb-[45vw] min-[480px]:mb-[28vw] min-[480px]:mt-[13vw] min-[530px]:mt-[18vw] min-[600px]:mt-[30vw] sm:mb-[9vw] sm:mt-[7vw] min-[720px]:mt-[12vw] md:mt-[10vw] min-[830px]:mb-[12vw] min-[830px]:mt-[15vw] min-[855px]:mt-[12vw] lg:mb-[15vw] min-[1100px]:mt-[15vw] min-[1244px]:mt-[20vw] xl:mt-[15vw] xl:max-w-none min-[1330px]:mt-[20vw]"
         >
           <h5
             class="bg-sc-white/70 font-heading text-xl font-bold text-sc-orange min-[480px]:text-2xl lg:text-3xl xl:mb-7 xl:text-6xl"
@@ -192,7 +198,7 @@
           <a href="mailto:lisa-marie.hoermann@scrumplicity.app">
             <UButton
               style="opacity: 100"
-              class="drop-shadow-sc-shadow hover:bg-orange-700 lg:px-5 lg:py-2 lg:text-base lg:font-bold"
+              class="drop-shadow-sc-shadow hover:bg-orange-700 lg:px-5 lg:py-2 lg:text-base lg:font-bold xl:text-xl"
             >
               {{ $t("homepage.contact_us") }}</UButton
             ></a
