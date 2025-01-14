3438806d6888a5232f065a57179c51f463dd3a1e
Marco Janderka <marco.janderka@htl.rennweg.at>
2025-01-14 17:08:26 +0000

    SC-26 Removed unused private channels

--- backend/routes/channels.php
+++ backend/routes/channels.php
@@ -9,22 +9,3 @@ Broadcast::routes(['middleware' => ['auth:sanctum']]);
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
     return (int) $user->id === (int) $id;
 });
-
-// Broadcast::channel('rooms.{roomId}', function (User $user, string $roomId) {
-//     error_log("User: " . $user->id . " Room: " . $roomId);
-//     $room = Room::where('id', $roomId)->first();
-//     if (!$room) {
-//         return false;
-//     }
-//     return $user->id === $room->user_id;
-// });
-
-// Private channel for rooms
-Broadcast::channel('rooms.{roomId}', function (User $user, Room $room) {
-    error_log("User: " . $user->id . " Room: " . $room . " Channel: private-rooms." . $room);
-
-    $authorized = $user->id === $room->user_id;
-    error_log($authorized . " = authorized");
-    
-    return $authorized;
-});
