f3a2efb46ba7477a0175974455d5c8ef37811014
Marco Janderka <marco.janderka@htl.rennweg.at>
2025-01-14 07:24:05 +0000

    SC-26 Delete unused import

--- backend/app/Http/Controllers/RoomController.php
+++ backend/app/Http/Controllers/RoomController.php
@@ -7,7 +7,6 @@ use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
 use App\Services\TimerService;
-use Illuminate\Support\Facades\Cache;
 
 class RoomController extends Controller
 {
