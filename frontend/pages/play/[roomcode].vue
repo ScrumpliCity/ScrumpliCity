bf9ab72e10ade7fef35ca81c61ff90de14dc8b9f
dasechtelima <0198@htl.rennweg.at>
2025-01-30 10:08:05 +0100

    SC-191: protoype of dropdown

--- frontend/pages/play/[roomcode].vue
+++ frontend/pages/play/[roomcode].vue
@@ -49,24 +49,100 @@ const teamName = ref("");
 
 async function submit() {
   await game.changeName(teamName.value);
-  await navigateTo(localRoute("play-members"));
+  if (roomAlreadyPlayed) {
+    //await navigateTo(localRoute("ready"));
+    console.log("TODO: navigate to ready");
+  } else {
+    await navigateTo(localRoute("play-members"));
+  }
+}
+
+//TODO: check if "last_play_start" is not null => Spiel noch nie gestartet
+const roomAlreadyPlayed = true;
+//TODO: get existing teams from backend
+const existingTeams = [
+  {
+    name: "ScrumpliCity",
+    members: [
+      "Felix Wollmann",
+      "Marco Janderka",
+      "Lisa-Marie Hörmann",
+      "Sophie Nemecek",
+    ],
+  },
+  {
+    name: "JourneyPlanner",
+    members: [
+      "Raven Burkard",
+      "Severin Rosner",
+      "Stefania Manastirska",
+      "Roman Krebs",
+    ],
+  },
+  {
+    name: "LinguExplorer",
+    members: [
+      "Tien Luong",
+      "Alexander Nems",
+      "Helena Stindl",
+      "Benjamin Bician",
+      "Britta Müller",
+    ],
+  },
+  {
+    name: "Specialbond",
+    members: [
+      "Anil Kapan",
+      "Arlon Labalan",
+      "Paula Wenighofer",
+      "Joavan Stojanovic",
+    ],
+  },
+];
+let selected = ref({});
+
+function changeDisplaySelected() {
+  selected.value = selected.value.name;
+  teamName.value = selected.value;
 }
 </script>
 <template>
   <div class="mt-10 flex h-full w-full flex-col items-center">
-    <h1 class="font-heading text-6xl font-bold text-sc-orange">
+    <h1
+      v-if="!roomAlreadyPlayed"
+      class="font-heading text-6xl font-bold text-sc-orange"
+    >
       {{ $t("join_room.team_title") }}
     </h1>
+    <h1 v-else class="font-heading text-6xl font-bold text-sc-orange">
+      {{ $t("join_room.choose_existing_team") }}
+    </h1>
     <input
+      v-if="!roomAlreadyPlayed"
       @keydown.enter="submit"
-      class="mb-12 mt-16 rounded-lg border-2 border-sc-black-400 py-8 text-center text-5xl font-medium drop-shadow-sc-shadow"
+      class="mt-16 rounded-lg border-2 border-sc-black-400 py-8 text-center text-5xl font-medium drop-shadow-sc-shadow"
       :placeholder="$t('join_room.team_name')"
       v-model="teamName"
     />
+    <USelectMenu
+      v-else
+      v-model="selected"
+      :options="existingTeams"
+      :placeholder="$t('join_room.team_name')"
+      class="mt-16 w-96 rounded-lg"
+      @change="changeDisplaySelected()"
+    >
+      <template #option="{ option: team }">
+        <div class="flex flex-col items-center">
+          <span class="truncate">{{ team.name }}</span>
+          <span>{{ team.members.join(", ") }}</span>
+        </div>
+      </template>
+    </USelectMenu>
     <button
       @click="submit"
       :disabled="!teamName"
-      class="w-72 cursor-pointer rounded-lg bg-sc-green py-6 text-center text-4xl font-bold text-sc-black drop-shadow-sc-shadow transition-colors hover:bg-sc-green-400 disabled:cursor-not-allowed disabled:bg-sc-black-400 disabled:text-sc-white"
+      class="mt-12 w-72 cursor-pointer rounded-lg bg-sc-green py-6 text-center text-4xl font-bold text-sc-black drop-shadow-sc-shadow transition-colors hover:bg-sc-green-400 disabled:cursor-not-allowed disabled:bg-sc-black-400 disabled:text-sc-white"
     >
       {{ $t("join_room.team_join") }}
     </button>
