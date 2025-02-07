a85c6708bfd9520eabff1d9b9e22706039e86e31
Marco Janderka <marco.janderka@htl.rennweg.at>
2025-02-07 15:18:00 +0000

    SC-61 Format

--- frontend/layouts/in-game.vue
+++ frontend/layouts/in-game.vue
@@ -5,7 +5,7 @@
       class="flex h-16 items-center justify-between px-14 shadow-[0px_4px_8px_0px_rgba(0,0,0,0.25)]"
     >
       <NuxtLinkLocale to="/">
-      <SvgMainLogo :font-controlled="false" class="h-10" />
+        <SvgMainLogo :font-controlled="false" class="h-10" />
       </NuxtLinkLocale>
       <ChangeLangButton></ChangeLangButton>
     </header>
