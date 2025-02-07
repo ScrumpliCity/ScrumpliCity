c72ecc34e301ecf4780842db21dcaf44fa302ccd
Marco Janderka <75068841+Pycxtv@users.noreply.github.com>
2025-02-07 16:10:52 +0100

    Update frontend/layouts/in-game.vue
    Co-authored-by: lima <88726854+dasechtelima@users.noreply.github.com>

--- frontend/layouts/in-game.vue
+++ frontend/layouts/in-game.vue
@@ -4,7 +4,9 @@
     <header
       class="flex h-16 items-center justify-between px-14 shadow-[0px_4px_8px_0px_rgba(0,0,0,0.25)]"
     >
+      <NuxtLinkLocale to="/">
       <SvgMainLogo :font-controlled="false" class="h-10" />
+      </NuxtLinkLocale>
       <ChangeLangButton></ChangeLangButton>
     </header>
     <div class="flex-1">
