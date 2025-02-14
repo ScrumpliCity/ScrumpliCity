25f6ad2f5b65fe5c60e8ae71514a908def616762
dasechtelima <0198@htl.rennweg.at>
2025-02-14 23:01:02 +0100

    SC-83: prototype for flex-wrap and text around images

--- frontend/pages/scrumplicity-guide.vue
+++ frontend/pages/scrumplicity-guide.vue
@@ -10,20 +10,25 @@
     <UIcon name="mdi:arrow-up" size="100" class="bg-sc-black" />
   </UButton>
   <div
-    class="flex h-fit min-h-screen flex-col bg-[url('/assets/svg/LegalNoticeDesktopBackground.svg')] bg-cover bg-[bottom_2rem_left_2rem] bg-no-repeat text-justify"
+    class="flex h-fit min-h-screen flex-col bg-[url('/assets/svg/ScrumpliCityGuideBackground.svg')] bg-cover bg-[bottom_2rem_left_2rem] bg-no-repeat text-justify"
   >
     <div
-      class="mb-[10vh] mt-32 flex w-[76.547vw] flex-col gap-2 self-center font-sans"
+      class="mb-[20vh] mt-32 flex w-[76.547vw] flex-col gap-2 self-center font-sans"
     >
-      <h1 class="mb-8 font-heading text-6xl font-bold text-sc-orange">
+      <h1
+        class="mb-8 text-center font-heading text-6xl font-bold text-sc-orange"
+      >
         {{ $t("scrumplicity_guide.title") }}
       </h1>
-      <div class="mb-5 flex flex-row items-center justify-between">
+      <!--
+      <div class="mb-5 flex flex-row flex-wrap items-center justify-between">
         <div class="flex-1 flex-col">
           <p class="mb-5 text-lg font-medium">
             {{ $t("scrumplicity_guide.overview") }}
           </p>
-          <p class="mb-3 font-heading text-3xl font-bold text-sc-orange">
+          <p
+            class="mb-3 text-left font-heading text-3xl font-bold text-sc-orange"
+          >
             {{ $t("scrumplicity_guide.introduction.title") }}
           </p>
           <p class="text-lg font-medium">
@@ -32,41 +37,57 @@
         </div>
         <SvgTutorSm class="ml-40 h-52 w-auto" :fontControlled="false" filled />
       </div>
-      <div class="mb-5 flex flex-row items-center justify-between">
-        <div class="flex-1 flex-col">
-          <p class="mb-3 font-heading text-3xl font-bold text-sc-orange">
-            {{ $t("scrumplicity_guide.prerequisites.title") }}
-          </p>
-          <p class="text-lg font-medium">
-            {{ $t("scrumplicity_guide.prerequisites.description") }}
-          </p>
-        </div>
-        <SvgCraftingTemplateIllustration
-          class="ml-[90px] h-28 w-auto"
+    -->
+      <div class="relative mb-5">
+        <SvgTutorSm
+          class="float-right mb-10 ml-40 mt-10 h-52 w-auto"
           :fontControlled="false"
           filled
         />
+        <p class="mb-5 text-lg font-medium">
+          {{ $t("scrumplicity_guide.overview") }}
+        </p>
+        <p
+          class="mb-3 text-left font-heading text-3xl font-bold text-sc-orange"
+        >
+          {{ $t("scrumplicity_guide.introduction.title") }}
+        </p>
+        <p class="text-lg font-medium">
+          {{ $t("scrumplicity_guide.introduction.description") }}
+        </p>
       </div>
-      <p class="mb-3 font-heading text-3xl font-bold text-sc-orange">
+      <p class="mb-3 text-left font-heading text-3xl font-bold text-sc-orange">
+        {{ $t("scrumplicity_guide.prerequisites.title") }}
+      </p>
+      <div class="relative mb-5">
+        <SvgCraftingTemplateIllustration
+          class="float-right mb-10 ml-16 h-28 w-auto"
+          :fontControlled="false"
+          filled
+        />
+        <p class="text-lg font-medium">
+          {{ $t("scrumplicity_guide.prerequisites.description") }}
+        </p>
+      </div>
+      <p class="mb-3 text-left font-heading text-3xl font-bold text-sc-orange">
         {{ $t("scrumplicity_guide.scrum_content.title") }}
       </p>
-      <div class="mb-32 flex">
-        <p class="flex-1 text-lg font-medium">
-          NOCH BROKEN
-          {{ $t("scrumplicity_guide.scrum_content.description") }}
-        </p>
+      <div class="relative mb-32">
         <SvgScrumProcessIllustration
-          class="float-right mb-[90px] ml-[90px] h-28 w-auto"
+          class="float-right mb-10 ml-[46px] h-28 w-auto"
           :fontControlled="false"
           filled
         />
+        <p class="text-lg font-medium">
+          {{ $t("scrumplicity_guide.scrum_content.description") }}
+        </p>
       </div>
       <h2
-        class="mb-8 self-start font-heading text-5xl font-bold text-sc-orange"
+        class="mb-8 self-start text-left font-heading text-5xl font-bold text-sc-orange"
       >
         {{ $t("scrumplicity_guide.step_by_step.title") }}
       </h2>
-      <div class="mb-5 flex flex-row items-center justify-between">
+      <div class="mb-5 flex flex-row flex-wrap items-center justify-between">
         <div class="mr-12 flex h-auto w-[541px] flex-col">
           <img
             src="/assets/png/role_page.png"
@@ -82,7 +103,9 @@
           {{ $t("scrumplicity_guide.step_by_step.description1") }}
         </p>
       </div>
-      <div class="mb-5 flex flex-row items-center justify-between">
+      <div
+        class="mb-5 flex flex-row flex-wrap-reverse items-center justify-between"
+      >
         <p class="flex-1 text-base font-medium">
           <span class="font-heading text-6xl font-bold text-sc-orange">2.</span>
           {{ $t("scrumplicity_guide.step_by_step.description2") }}
@@ -98,7 +121,7 @@
           </p>
         </div>
       </div>
-      <div class="mb-5 flex flex-row items-center justify-between">
+      <div class="mb-5 flex flex-row flex-wrap items-center justify-between">
         <div class="mr-12 flex h-auto w-[541px] flex-col">
           <img
             src="/assets/png/role_page.png"
@@ -114,7 +137,9 @@
           {{ $t("scrumplicity_guide.step_by_step.description3") }}
         </p>
       </div>
-      <div class="mb-5 flex flex-row items-center justify-between">
+      <div
+        class="mb-5 flex flex-row flex-wrap-reverse items-center justify-between"
+      >
         <p class="flex-1 text-base font-medium">
           <span class="font-heading text-6xl font-bold text-sc-orange">4.</span>
           {{ $t("scrumplicity_guide.step_by_step.description4") }}
@@ -130,7 +155,7 @@
           </p>
         </div>
       </div>
-      <div class="mb-5 flex flex-row items-center justify-between">
+      <div class="mb-5 flex flex-row flex-wrap items-center justify-between">
         <div class="mr-12 flex h-auto w-[541px] flex-col">
           <img
             src="/assets/png/role_page.png"
@@ -146,7 +171,9 @@
           {{ $t("scrumplicity_guide.step_by_step.description5") }}
         </p>
       </div>
-      <div class="mb-32 flex flex-row items-center justify-between">
+      <div
+        class="mb-32 flex flex-row flex-wrap-reverse items-center justify-between"
+      >
         <p class="flex-1 text-base font-medium">
           <span class="font-heading text-6xl font-bold text-sc-orange">6.</span>
           {{ $t("scrumplicity_guide.step_by_step.description6") }}
@@ -163,11 +190,16 @@
         </div>
       </div>
       <h3
-        class="mb-8 self-start font-heading text-5xl font-bold text-sc-orange"
+        class="mb-8 self-start text-left font-heading text-5xl font-bold text-sc-orange"
       >
         {{ $t("scrumplicity_guide.learners_title") }}
       </h3>
-      <div class="mb-32 flex flex-row items-center justify-between">
+      <div class="relative mb-5">
+        <SvgLearnerSm
+          class="float-right mb-10 ml-40 mt-10 h-[188px] w-auto"
+          :fontControlled="false"
+          filled
+        />
         <i18n-t
           keypath="scrumplicity_guide.learners_description"
           tag="p"
@@ -175,7 +207,7 @@
           scope="global"
         >
           <template #join_steps>
-            <ul class="ml-6 list-disc">
+            <ul class="ml-6 list-disc text-left">
               <li>
                 {{ $t("scrumplicity_guide.join_steps.step1") }}
               </li>
@@ -191,11 +223,6 @@
             </ul>
           </template>
         </i18n-t>
-        <SvgLearnerSm
-          class="ml-40 h-[188px] w-auto"
-          :fontControlled="false"
-          filled
-        />
       </div>
       <div class="flex w-full justify-center">
         <NuxtLink :to="localeRoute('role')">
